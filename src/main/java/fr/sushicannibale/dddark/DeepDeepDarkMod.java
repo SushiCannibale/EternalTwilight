c2ac3e14ba4c5bdb0d190e4b5ba22c01d2aaff57
SushiCannibale_ <marcochat333@gmail.com>
2022-07-01 20:02:52 +0200

    EchoWand behaviour

--- src/main/java/fr/sushicannibale/dddark/DeepDeepDarkMod.java
+++ src/main/java/fr/sushicannibale/dddark/DeepDeepDarkMod.java
@@ -1,22 +1,16 @@
 package fr.sushicannibale.dddark;
 
-import fr.sushicannibale.dddark.dimensions.ModDimensions;
 import fr.sushicannibale.dddark.events.ModCommonEvents;
 import fr.sushicannibale.dddark.sounds.ModSounds;
+import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.entity.monster.warden.Warden;
+import net.minecraft.world.entity.monster.warden.WardenAi;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(DeepDeepDarkMod.MODID)
@@ -39,7 +33,7 @@ public class DeepDeepDarkMod
         ModItems.ITEM_REGISTRY.register(bus);
         ModSounds.SOUND_EVENTS.register(bus);
 
-        ModDimensions.printRegister();
+//        ModDimensions.printRegister();
 
         MinecraftForge.EVENT_BUS.register(ModCommonEvents.class);
     }
